ad4317a5520652908cd8c5be5a17cfaa5135a5b3
Katrina Hall <katrina.m.hall@gmail.com>
2017-05-24 15:07:58 -0400

    added queue initializing, process will shutdown gracefully

--- src/vip/batch_address_test_tool/core.clj
+++ src/vip/batch_address_test_tool/core.clj
@@ -1,10 +1,14 @@
 (ns vip.batch-address-test-tool.core
-  (:require [clojure.tools.logging :as log]))
+  (:require [clojure.tools.logging :as log]
+            [vip.batch-address-test-tool.queue :as q]))
 
 
 (defn -main [& args]
   (let [id (java.util.UUID/randomUUID)]
     (log/info "VIP Batch Address Test Tool starting up. ID:" id)
+    (q/initialize)
+    (q/publish {:id id :event "starting"} "batch-address.status")
     (.addShutdownHook (Runtime/getRuntime)
                       (Thread. (fn []
+                                 (q/publish {:id id :event "stopping"} "batch-address.status")
                                  (log/info "VIP Batch Address Test Tool shutting down..."))))))
