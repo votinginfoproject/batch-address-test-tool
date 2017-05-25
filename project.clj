b6899690d76607c8dff29ce9dad63211e306b95a
Katrina Hall <katrina.m.hall@gmail.com>
2017-05-25 13:15:54 -0400

    updated dependencies

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject batch-address-test-tool "0.1.0-SNAPSHOT"
   :description "An address verification tool"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [turbovote.resource-config "0.2.0"]
-                 [com.novemberain/langohr "3.5.1"]
-                 [org.apache.logging.log4j/log4j-core "2.5"]
-                 [org.apache.logging.log4j/log4j-slf4j-impl "2.5"]
-                 [org.slf4j/slf4j-api "1.7.20"]
+                 [turbovote.resource-config "0.2.1"]
+                 [com.novemberain/langohr "4.0.0"]
+                 [org.apache.logging.log4j/log4j-core "2.8.2"]
+                 [org.apache.logging.log4j/log4j-slf4j-impl "2.8.2"]
+                 [org.slf4j/slf4j-api "1.7.25"]
                  [org.clojure/tools.logging "0.3.1"]
                  [com.climate/clj-newrelic "0.2.1"]]
   :plugins [[com.pupeno/jar-copier "0.4.0"]]
